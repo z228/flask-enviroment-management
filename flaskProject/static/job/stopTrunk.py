367012973413c5b9746f529abe6359d8ca18c9dc
z228 <657676819@qq.com>
2021-08-30 16:30:15 +0800

    add job

---
+++ flaskProject/static/job/stopTrunk.py
@@ -0,0 +1,23 @@
+import win32api as api
+import win32console as con
+import os
+
+# 向某个进程发送crtl+c指令
+def send_ctrl_c(pid):
+    con.FreeConsole()
+    if con.AttachConsole(int(pid)) == None:
+        api.SetConsoleCtrlHandler(None, 1)
+        api.GenerateConsoleCtrlEvent(con.CTRL_C_EVENT, 0)
+        api.Sleep(2000)
+        con.FreeConsole()
+        api.SetConsoleCtrlHandler(None, 0)
+
+# 通过host+port获取进程pid
+def get_pid_from_port(port):
+    res = os.popen(f'netstat -ano |findstr "{port}"').readlines()
+    for i in res:
+        if i.split()[-2] == 'LISTENING':
+            print(i)
+            return i.split()[-1]
+
+send_ctrl_c(get_pid_from_port("8080"))
