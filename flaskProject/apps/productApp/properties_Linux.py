a0a133d089d0e0248979dcee8c2a3b512bf9b5ae
z228 <657676819@qq.com>
2022-02-14 18:12:57 +0800

    Update .gitignore

--- flaskProject/apps/productApp/properties_Linux.py
+++ flaskProject/apps/productApp/properties_Linux.py
@@ -1,30 +1,35 @@
 env_list = {
   "version": {
-    "v8.7": [
-      "/opt/87",
-      "8087",
-      "bihome"
+    "v8.6": [
+      "/opt/86",
+      "8921",
+      "bihome Export UIauto"
     ],
     "v9.0": [
       "/opt/90",
-      "8090",
-      "bihome Export"
+      "8921",
+      "bihome Export UIauto"
     ],
     "v9.2.1": [
       "/opt/921",
       "8921",
-      "bihome Export"
+      "bihome Export UIauto"
     ],
-    "v9.4.1":[
-      "/opt/941",
+    "v9.4": [
+      "/opt/94",
+      "8921",
+      "bihome Export UIauto"
+    ],
+    "custom_v9.2.3_huawei_dis": [
+      "/opt/Yonghong_Z-Suite_dis_923",
+      "8921",
+      "bihome Export UIauto"
+    ],
+    "custom_v9.4.1_huawei_xian": [
+      "/opt/Yonghong_Z-Suite_xian_941",
       "8941",
-      "bihome Export uiAuto"
-    ],
-    "develop":[
-      "/opt/trunk",
-      "8080",
-      "bihome Export uiAuto"
-    ],
+      "bihome Export UIauto"
+    ]
   },
   "mid_path": ""
 }
