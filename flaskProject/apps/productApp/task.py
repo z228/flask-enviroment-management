80d8e98c9873811c0f270fd22f13da887107aa47
65767 <657676819@qq.com>
2022-05-16 11:57:42 +0800

    修改日志的handler

--- flaskProject/apps/productApp/task.py
+++ flaskProject/apps/productApp/task.py
@@ -1,11 +1,21 @@
 import os
 from . import product
 import shutil
+import logging
+from logging.handlers import TimedRotatingFileHandler
 
 to_path = ['D:/old_version/8.6/', 'D:/old_version/8.8/', 'D:/old_version/9.0/', 'D:/old_version/9.1/',
            'D:/old_version/9.2/', 'D:/old_version/9.2.1/', 'D:/old_version/9.3/', 'D:/old_version/trunk/']
 path = 'Yonghong_Z-Suite/Yonghong'
 productAction = product.ProductAction()
+
+log_path = f'{os.getcwd()}/logs/task.log'
+handler = TimedRotatingFileHandler(log_path, when="D", interval=1, backupCount=10)  # 设置日志字符集和存储路径名字
+logging_format = logging.Formatter(  # 设置日志格式
+    '%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(lineno)s - %(message)s')
+handler.setFormatter(logging_format)
+logging.getLogger('').addHandler(handler)
+
 
 def clean_jar():
     for i in to_path:
@@ -32,3 +42,7 @@ def Jacoco_change_Jar():
     work_dir = r'D:\SVN\trunk\test\assetExecute'
     os.chdir(work_dir)
     os.system('ant test report')
+
+
+def test_task():
+    logging.info("这个是测试task log")
