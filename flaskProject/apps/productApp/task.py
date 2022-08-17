763fb5a930549fdee9da9d824b341492b44d6f9f
657676819@qq.com <657676819@qq.com>
2022-08-17 15:04:27 +0800

    change message of email

--- flaskProject/apps/productApp/task.py
+++ flaskProject/apps/productApp/task.py
@@ -135,7 +135,7 @@ def try_copy(v, ip_today, ip_134_today):
         path0 = ip_134_today.replace('/mnt/134/productJar','134\git-package')
         path1 = ip_today.replace('/home','187')
         content = f"{path0} update to {path1}"
-        content0 = "</p><p><small>取包的路径：\\\\192.168.0.187\share（账户密码：<b>tkl-share/9926</b>）</small></p>"
+        content0 = "</p><p><small>取包的路径：<br>#1.\\\\192.168.0.187\share（账户密码：<b>tkl-share/9926</b>）</small><br>#2.\\\\192.168.0.141\productJar（账户密码：<b>cdqa/yonghong@123</b>）</p>"
         task_logger.info(content)
         send("zengchenglong@yonghongtech.com", subject, part0 + content + content0 + part2)
     except PermissionError:
