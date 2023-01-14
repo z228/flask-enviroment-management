bdd747e75cd9394510d35c7babf69e5e0c385d6f
657676819@qq.com <657676819@qq.com>
2023-01-14 22:24:07 +0800

    code review

--- flaskProject/update_reload_flask.sh
+++ flaskProject/update_reload_flask.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 res=`lsof -i:5000`
-port=`lsof -i:5000|awk '{print $2}'|grep "[0-9]"`
-cmd=`lsof -i:5000|awk '{print $1}'|grep "[^COMMAND]"`
+#port=$(lsof -i:5000 | awk '{print $2}' | grep "[0-9]")
+cmd=`lsof -i:5000 | awk '{print $1}' | grep "[^COMMAND]"`
 echo "$res"
 # echo "kill -9 $port"
 echo "$cmd"
-if [ $cmd = "python3" ]
-then
-cd /opt/my-first-flask/flaskProject
-echo "更新本地代码"
-git pull
-echo "关闭flask"
-kill -9 $port
-echo "后台运行flask"
-nohup python3  -u app.py > run.log 2>&1 &
+if [ $cmd = "python3" ]; then
+    cd /opt/my-first-flask/flaskProject
+    echo "更新本地代码"
+    git pull
+    echo "关闭flask"
+    lsof -i:5000 | awk '{print $2}' | grep "[0-9]" | xargs kill -9
+    echo "后台运行flask"
+    nohup python3 -u app.py >run.log 2>&1 &
+else
+    echo "flask没有在运行"
+    echo "后台运行flask"
+    nohup python3 -u app.py >run.log 2>&1 &
 fi
 #kill -9 port
